c4d1b73d3b5061a72315405a1188edb12d1a0f19
Megs1307 <94031263+Megs1307@users.noreply.github.com>
2023-09-12 14:00:39 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,4 +1,5 @@
 package bunchbysoh;
+
 public class Main {
     static class CountsBySoH {
         public int healthy = 0;
@@ -17,16 +18,14 @@ public class Main {
         return counts;
     }
 
-    // Calculate State of Health (SoH) as a percentage
     static double calculateSoH(int presentCapacity, int ratedCapacity) {
         return (double) presentCapacity / ratedCapacity * 100.0;
     }
 
-    // Classify batteries based on SoH
     static void classifyBattery(CountsBySoH counts, double soh) {
         if (soh > 80.0) {
             counts.healthy++;
-        } else if (soh >= 63.0) {
+        } else if (soh >= 65.0) {
             counts.exchange++;
         } else {
             counts.failed++;
@@ -37,17 +36,13 @@ public class Main {
         System.out.println("Counting batteries by SoH...\n");
         int[] presentCapacities = {115, 118, 80, 95, 91, 77};
         CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-        //assert counts.healthy == 2;
-        //assert counts.exchange == 3;
-        //assert counts.failed == 1;
-        System.out.println("Healthy: " + counts.healthy);
-        System.out.println("Exchange: " + counts.exchange);
-        System.out.println("Failed: " + counts.failed);
+        assert(counts.healthy == 2);
+        assert(counts.exchange == 3);
+        assert(counts.failed == 1);
         System.out.println("Done counting :)\n");
     }
 
     public static void main(String[] args) {
-      //assertAssertionsEnabled();
         testBucketingByHealth();
     }
 }
